22db6a7b42c446d279b2b9746b11d7aa1bab079a
Langello <gastonchavo@gmail.com>
2023-11-10 21:12:36 -0300

    [creacion componentes para el home del lado del consumidor]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ProfesionalesComponent } from './profesionales/profesionales.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ProfesionalesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
